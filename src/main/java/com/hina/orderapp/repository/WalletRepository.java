1b174063f2b051df7da8be905d6deed742ce5891
hina zakir <bcs163015@cust.pk>
2020-06-19 15:16:26 +0500

    final commit

---
+++ src/main/java/com/hina/orderapp/repository/WalletRepository.java
@@ -0,0 +1,12 @@
+package com.hina.orderapp.repository;
+
+import com.hina.orderapp.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet,Long> {
+    List<Wallet> findAllByOrderByPriority();
+}
