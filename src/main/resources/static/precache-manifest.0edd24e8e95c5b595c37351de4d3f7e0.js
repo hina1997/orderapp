d74cf4c5b4a95e1fe9fdff2772c2e64f9a92bfa9
hina zakir <bcs163015@cust.pk>
2020-06-20 09:19:35 +0500

    final commit

---
+++ src/main/resources/static/precache-manifest.0edd24e8e95c5b595c37351de4d3f7e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c33eb8260c9f4e99031e31aeab5b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73b68ccb76029616f60",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7ffa4aaa59f58204317",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "e73b68ccb76029616f60",
+    "url": "/static/js/2.4aed09eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4aed09eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ffa4aaa59f58204317",
+    "url": "/static/js/main.9758038b.chunk.js"
+  },
+  {
+    "revision": "4429e2b35fda9818aebf",
+    "url": "/static/js/runtime-main.a797f5fe.js"
+  }
+]);
